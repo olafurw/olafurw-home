ef2faddbaca8bc792216ba0fd0bcdd8a9057e9d3
Ólafur Waage <olafurw@waage.tern.is>
2013-03-12 15:51:34 +0000

    Fixed if run with no arguments. Added a -p to print the directory, useful for scripts that use this tool.

--- tools/qwe.sh
+++ tools/qwe.sh
@@ -1,49 +1,73 @@
 qwe () {
-        dataFile=$HOME/.qwe.data
-        currentDirectory=$(pwd)
-        touch $dataFile
+	dataFile=$HOME/.qwe.data
+	currentDirectory=$(pwd)
+	touch $dataFile
 
-        if [ $1 == "-h" -a $# == 1 ]; then
-                echo "Usage:"
-                echo "qwe -h : Help"
-                echo "qwe -l : List of saved tags"
-                echo "qwe -a name : Add a tag named name with the current folder"
-                echo "qwe -d name : Remove a tag named name"
-        elif [ $1 == "-a" -a $# == 2 ]; then
-                touch $dataFile
-                echo -e $2"\t"$currentDirectory >> $dataFile
+    harg=""
+    if [ $# == 0 ]; then
+        harg="-h"
+    elif [ $# == 1 -a $1 == "-h" ]; then
+        harg="-h"
+    fi
 
-        elif [ $1 == "-l" -a $# == 1 ]; then
-                cat $dataFile
-        elif [ $1 == "-d" -a $# == 2 ]; then
-                data=$(cat $dataFile)
-                tagList=$(echo "$data"|sed 's/[ \t].*//')
-                lineNumberInfo=$(echo "$tagList"|grep -ne "^$2$")
+    if [ "$harg" == "-h" ]; then
+		echo "Usage:"
+        echo "qwe name : Traverse to directory tagged name"
+		echo "qwe -h : Help"
+		echo "qwe -l : List of saved tags"
+		echo "qwe -a name : Add a tag called name with the current folder"
+		echo "qwe -d name : Remove a tag called name"
+        echo "qwe -p name : Print the directory tagged with name"
+	elif [ $1 == "-a" -a $# == 2 ]; then
+		touch $dataFile
+		echo -e $2"\t"$currentDirectory >> $dataFile
+	elif [ $1 == "-l" -a $# == 1 ]; then
+		cat $dataFile
+	elif [ $1 == "-d" -a $# == 2 ]; then
+		data=$(cat $dataFile)
+		tagList=$(echo "$data"|sed 's/[ \t].*//')
+		lineNumberInfo=$(echo "$tagList"|grep -ne "^$2$")
 
-                dataCount=$(echo "$lineNumberInfo"|wc -m)
+		dataCount=$(echo "$lineNumberInfo"|wc -m)
 
-                if [ $dataCount -gt 1 ]; then
-                        lineNumber=$(echo $lineNumberInfo|cut -d ":" -f1)
+		if [ $dataCount -gt 1 ]; then
+			lineNumber=$(echo $lineNumberInfo|cut -d ":" -f1)
 
-                        sed -i -e "$lineNumber"'d' $dataFile
-                else
-                        echo "Tag $2 not found"
-                fi
-        elif [ $# == 1 ]; then
-                data=$(cat $dataFile)
-                tagList=$(echo "$data"|sed 's/[ \t].*//')
-                lineNumberInfo=$(echo "$tagList"|grep -ne "^$1$")
+			sed -i -e "$lineNumber"'d' $dataFile
+		else
+			echo "Tag $2 not found"
+		fi
+    elif [ $1 == "-p" -a $# == 2 ]; then
+        data=$(cat $dataFile)
+        tagList=$(echo "$data"|sed 's/[ \t].*//')
+        lineNumberInfo=$(echo "$tagList"|grep -ne "^$2$")
 
-                dataCount=$(echo "$lineNumberInfo"|wc -m)
+        dataCount=$(echo "$lineNumberInfo"|wc -m)
 
-                if [ $dataCount -gt 1 ]; then
-                        lineNumber=$(echo $lineNumberInfo|cut -d ":" -f1)
-                        line=$(head -$lineNumber $dataFile|tail -1)
-                        directory=$(echo $line|sed 's/^[a-zA-Z0-9\.-]*\ *//'|sed 's/ /\\ /g')
+        if [ $dataCount -gt 1 ]; then
+            lineNumber=$(echo $lineNumberInfo|cut -d ":" -f1)
+            line=$(head -$lineNumber $dataFile|tail -1)
+            directory=$(echo $line|sed 's/^[a-zA-Z0-9\.-]*\ *//'|sed 's/ /\\ /g')
 
-                        eval cd $directory
-                else
-                        echo "Tag $1 not found"
-                fi
-        fi
+            echo $directory
+         else
+            echo "Tag $2 not found"
+         fi
+	elif [ $# == 1 ]; then
+		data=$(cat $dataFile)
+		tagList=$(echo "$data"|sed 's/[ \t].*//')
+		lineNumberInfo=$(echo "$tagList"|grep -ne "^$1$")
+
+		dataCount=$(echo "$lineNumberInfo"|wc -m)
+
+		if [ $dataCount -gt 1 ]; then
+			lineNumber=$(echo $lineNumberInfo|cut -d ":" -f1)
+			line=$(head -$lineNumber $dataFile|tail -1)
+			directory=$(echo $line|sed 's/^[a-zA-Z0-9\.-]*\ *//'|sed 's/ /\\ /g')
+
+			eval cd $directory
+		else
+			echo "Tag $1 not found"
+		fi
+    fi
 }
